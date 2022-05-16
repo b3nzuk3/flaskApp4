a8ee30523e174e37669d9701f65b718831d972c3
b3nzuk3 <hussenito7@gmail.com>
2022-05-17 02:32:24 +0300

    added registration template

---
+++ blog/forms.py
@@ -0,0 +1,8 @@
+from wtf import Form, BooleanField, StringField, PasswordField, validators
+
+
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.length(min=2, max=20)])
+    email = StringField('Email', [validators.length(min=4, max=35 )])
+    password = PasswordField('New Password', [validators.DataRequired(), validators.EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField('Repeat Password')
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from blog import app
-from flask import render_template, url_for
+from flask import render_template, url_for, request
+from blog.forms import RegistrationForm
 
 
 @app.route('/')
@@ -8,3 +9,12 @@ def home():
     post = 'Hello World'
 
     return render_template('home.html', post = post)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        flash('Thanks for Registration')
+        return redirect(url_for('home'))
+
+    return render_template('register.html', form=form)
