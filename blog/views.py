63186d952a98d61f35458978756125e6bb7d8338
b3nzuk3 <hussenito7@gmail.com>
2022-05-17 03:03:35 +0300

    created a login template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from blog import app
 from flask import render_template, url_for, redirect, flash, request
-from blog.forms import RegistrationForm
+from blog.forms import RegistrationForm, LoginForm
 
 
 @app.route('/')
@@ -16,5 +16,17 @@ def register():
     if request.method == 'POST' and form.validate():
         flash('Thanks for Registration', 'success')
         return redirect(url_for('home'))
+
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        if request.form['email'] != 'admin@blog.com' or request.form['password'] != 'secret'
+            error = 'Invalid Credentials'
+        else:
+            flash('Thanks for Registration', 'success')
+            return redirect(url_for('home'))
 
     return render_template('register.html', form=form)
