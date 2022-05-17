96d570e2f15633420c57479f20374b2e92d2f6ac
b3nzuk3 <hussenito7@gmail.com>
2022-05-17 03:45:49 +0300

    added my models

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app=Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///my.sqlite"
+db = SQLAlchemy(app)
 
 from blog import views
