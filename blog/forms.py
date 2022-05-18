56b34e1b79c0efa981c1636224eef7b9f62801df
b3nzuk3 <hussenito7@gmail.com>
2022-05-18 12:38:48 +0300

    User is able to Login and Logout

--- blog/forms.py
+++ blog/forms.py
@@ -1,16 +1,17 @@
 from wtforms import Form, BooleanField, StringField, PasswordField,TextAreaField, validators
-
+from flask_login import current_user
+from blog.models import User
 
 class RegistrationForm(Form):
-    username = StringField('Username', [validators.length(min=2, max=20)])
-    email = StringField('Email', [validators.length(min=4, max=35 )])
+    username = StringField('Username', [validators.length(min=2, max=20),validators.DataRequired()])
+    email = StringField('Email', [validators.length(min=4, max=35 ),validators.DataRequired()])
     password = PasswordField('New Password', [validators.DataRequired(), validators.EqualTo('confirm', message='Passwords must match')])
     confirm = PasswordField('Repeat Password')
 
 class LoginForm(Form):
-    email = StringField('Email', [validators.length(min=4, max=35 )])
+    email = StringField('Email', [validators.length(min=4, max=35 ),validators.DataRequired()])
     password = PasswordField('New Password', [validators.DataRequired()])
 
 class BlogForm(Form):
-    title = StringField('Title', [validators.length(min=4, max=50)])
+    title = StringField('Title', [validators.length(min=4, max=50),validators.DataRequired()])
     content = TextAreaField('Content', [validators.DataRequired()])
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from blog import app, db, bcrypt
 from flask import render_template, url_for, redirect, flash, request
 from blog.forms import RegistrationForm, LoginForm, BlogForm
 from blog.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/home')
@@ -12,6 +13,8 @@ def home():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm(request.form)
     if request.method == 'POST' and form.validate():
         hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -25,17 +28,27 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm(request.form)
     error = None
     if request.method == 'POST' and form.validate():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
             return redirect(url_for('home'))
         else:
             flash('Login unsuccessful please try again', 'danger')
     return render_template('login.html', form=form, error=error)
 
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
 @app.route('/newblog', methods=['GET', 'POST'])
+@login_required
 def newblog():
     form = BlogForm(request.form)
     if request.method == 'POST' and form.validate():
@@ -45,3 +58,8 @@ def newblog():
         flash('Blog created', 'success')
         return redirect(url_for('home'))
     return render_template('newblog.html', form=form,)
+
+@app.route("/account")
+def account():
+
+    return render_template('account.html')
